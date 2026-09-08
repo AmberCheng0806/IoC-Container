cf6e3ac893d4462ada1d170f703cda15453820e8
AmberCheng0806 <qaz29zaq34@gmail.com>
2026-09-08 17:24:56 +0800

    實作服務解析與建構式依賴注入

--- IoC-Container/ServiceProvider.cs
+++ IoC-Container/ServiceProvider.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using IoC_Container.Attributes;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,69 +10,73 @@ using System.Threading.Tasks;
 
 namespace IoC_Container
 {
-    public class ServiceProvider
+    public class ServiceProvider : IServiceProvider
     {
-        public Dictionary<Type, List<ServiceItem>> KeyValuePairs;
-        public ServiceProvider(Dictionary<Type, List<ServiceItem>> keyValuePairs)
+        public Dictionary<Type, List<ServiceDescriptor>> KeyValuePairs;
+        public Dictionary<ServiceDescriptor, object> Instances = new Dictionary<ServiceDescriptor, object>();
+        public ServiceProvider(Dictionary<Type, List<ServiceDescriptor>> keyValuePairs)
         {
             KeyValuePairs = keyValuePairs;
         }
-        public T Get<T>()
+        private T Get<T>()
         {
-            List<ServiceItem> serviceItems = KeyValuePairs[typeof(T)];
-            ServiceItem serviceItem = serviceItems.FirstOrDefault();
-            if (serviceItem.LifeTimeEnum == LifeTimeEnum.Singleton && serviceItem.Item != null) return (T)serviceItem.Item;
-            object item;
-            if (serviceItem.Func != null) { item = serviceItem.Func(this); }
-            else { item = CreateInstance(serviceItem.Type); }
-            if (serviceItem.LifeTimeEnum == LifeTimeEnum.Singleton)
-            {
-                serviceItem.Item = item;
-            }
-            return (T)item;
+            return (T)Get(typeof(T));
         }
-        public List<T> GetAll<T>()
+        private List<T> GetList<T>()
         {
-            List<ServiceItem> serviceItems = KeyValuePairs[typeof(T)];
-            return serviceItems.Select(x =>
-            {
-                if (x.LifeTimeEnum == LifeTimeEnum.Singleton && x.Item != null) return (T)x.Item;
-                var item = (T)CreateInstance(x.Type);
-                if (x.LifeTimeEnum == LifeTimeEnum.Singleton) x.Item = item;
-                return item;
-            }).ToList();
-
+            return (List<T>)GetList(typeof(T));
         }
-        public object Get(Type type)
+        private object Get(Type type)
         {
-            List<ServiceItem> serviceItems = KeyValuePairs[type];
-            ServiceItem serviceItem = serviceItems.FirstOrDefault();
-            if (serviceItem.LifeTimeEnum == LifeTimeEnum.Singleton && serviceItem.Item != null) return serviceItem.Item;
-            object item;
-            if (serviceItem.Func != null) { item = serviceItem.Func(this); }
-            else { item = CreateInstance(serviceItem.Type); }
-            if (serviceItem.LifeTimeEnum == LifeTimeEnum.Singleton)
-            {
-                serviceItem.Item = item;
-            }
-            return item;
+            ServiceDescriptor serviceItem = serviceItems.LastOrDefault();
+            return GetInstance(serviceItem, type);
         }
-        public object GetAll(Type type)     // object => List<object>  /   object => List<IPeople>
+        private object Get(Type type, string key)
         {
-            List<ServiceItem> serviceItems = KeyValuePairs[type];
+            List<ServiceDescriptor> serviceItems = KeyValuePairs[type];
+            ServiceDescriptor serviceItem = serviceItems.LastOrDefault(x => x.ServiceKey != null && x.ServiceKey.ToString() == key);
+            return GetInstance(serviceItem, type);
+        }
+        private object GetList(Type type)     // object => List<object>  /   object => List<IPeople>
+        {
+            List<ServiceDescriptor> serviceItems = KeyValuePairs[type];
             Type listType = typeof(List<>).MakeGenericType(type);
             IList list = (IList)Activator.CreateInstance(listType); //list<type>
             serviceItems.ForEach(x =>
             {
-                if (x.LifeTimeEnum == LifeTimeEnum.Singleton && x.Item != null) { list.Add(x.Item); }
-                else
-                {
-                    var item = CreateInstance(x.Type);
-                    if (x.LifeTimeEnum == LifeTimeEnum.Singleton) x.Item = item;
-                    list.Add(item);
-                }
+                object item = GetInstance(x, type);
+                list.Add(item);
             });
             return list;
+        }
+        private object GetInstance(ServiceDescriptor descriptor, Type serviceType)
+        {
+            if (descriptor.Lifetime == ServiceLifetime.Singleton && Instances.ContainsKey(descriptor))
+            {
+                return Instances[descriptor];
+            }
+            object item;
+            if (descriptor.ImplementationInstance != null)
+            {
+                item = descriptor.ImplementationInstance;
+            }
+            else if (descriptor.ImplementationFactory != null)
+            {
+                item = descriptor.ImplementationFactory(this);
+            }
+            else if (descriptor.ImplementationType != null)
+            {
+                item = CreateInstance(descriptor.ImplementationType);
+            }
+            else
+            {
+                item = CreateInstance(descriptor.KeyedImplementationType);
+            }
+            if (descriptor.Lifetime == ServiceLifetime.Singleton)
+            {
+                Instances[descriptor] = item;
+            }
+            return item;
         }
 
         private object CreateInstance(Type type)
@@ -85,31 +91,40 @@ namespace IoC_Container
                     var parmInstances = parms.Select(x =>
                     {
                         //Ienumerable<Animal>
-                        if (x.ParameterType.IsGenericType && x.ParameterType.GetInterfaces().Any(y => y.GetGenericTypeDefinition() == typeof(IEnumerable<>)))
+                        if (x.ParameterType.IsGenericType && (x.ParameterType.GetInterfaces().Any(y => y.IsGenericType && y.GetGenericTypeDefinition() == typeof(IEnumerable<>)) || x.ParameterType.GetGenericTypeDefinition() == typeof(IEnumerable<>)))
                         {
                             Type argument = x.ParameterType.GetGenericArguments()[0];
                             if (!KeyValuePairs.ContainsKey(argument)) return null;
-                            return GetAll(argument);
+                            return GetList(argument);
                         }
 
 
                         //空泛型介面 PCgame<>
-                        // 你拿到的: IPlatformGame<GTA> (無法new interface出來) 實際上要注入的: PCGame<GTA>
+                        // 拿到的: IPlatformGame<GTA> (無法new interface出來) 實際上要注入的: PCGame<GTA>
                         // 需要從IPlatformGame 反找到PCGame
                         // 再將 GTA 放入 PCGame
                         // 最後才能 new PCGame
                         if (x.ParameterType.IsGenericType && KeyValuePairs.ContainsKey(x.ParameterType.GetGenericTypeDefinition()))
                         {
                             Type genericType = x.ParameterType.GetGenericTypeDefinition();
-                            Type implentmentType = KeyValuePairs[genericType].Last().Type;
-                            Type argument = x.ParameterType.GetGenericArguments()[0];
-                            Type result = implentmentType.MakeGenericType(argument); //PCgame<GTA>
+                            Type implentmentType = KeyValuePairs[genericType].Last().ImplementationType != null ?
+                           KeyValuePairs[genericType].Last().ImplementationType : KeyValuePairs[genericType].Last().KeyedImplementationType;
+                            Type[] arguments = x.ParameterType.GetGenericArguments();
+                            Type result = implentmentType.MakeGenericType(arguments); //PCgame<GTA>
                             return CreateInstance(result);
                         }
                         //其他類別+介面
                         if (!KeyValuePairs.ContainsKey(x.ParameterType)) return null;
-                        Type implentment = KeyValuePairs[x.ParameterType].Last().Type;
-                        return CreateInstance(implentment);
+                        //Type implentment = KeyValuePairs[x.ParameterType].Last().ImplementationType;
+                        //return CreateInstance(implentment);
+
+                        //從建構元參數拿注入實體key
+                        var key = x.GetCustomAttribute<GetInstanceAttribute>()?.ServiceKey;
+                        if (!string.IsNullOrEmpty(key))
+                        {
+                            return Get(x.ParameterType, key);
+                        }
+                        return Get(x.ParameterType);
                     }).ToArray();
                     if (parmInstances.Any(x => x == null)) continue;
                     item = Activator.CreateInstance(type, parmInstances);
@@ -121,6 +136,24 @@ namespace IoC_Container
                 return item;
             }
             return null;
+        }
+
+        public object GetService(Type serviceType)
+        {
+            if (!KeyValuePairs.ContainsKey(serviceType))
+            {
+                return null;
+            }
+            if (KeyValuePairs[serviceType].Count > 1)
+            {
+                return GetList(serviceType);
+            }
+            return Get(serviceType);
+        }
+
+        public T GetService<T>()
+        {
+            return (T)GetService(typeof(T));
         }
     }
 }
