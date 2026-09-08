a4077ca053ab37152115ff86904de4d8dee8bb1d
AmberCheng0806 <qaz29zaq34@gmail.com>
2026-09-08 17:26:36 +0800

    實作PresenterFactory建立與注入Presenter

---
+++ IoC-Container/Factory/PresenterFactory.cs
@@ -0,0 +1,33 @@
+﻿using IoC_Container;
+using IoC_Container.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IoC_Container.Factory
+{
+    public class PresenterFactory : IPresenterFactory
+    {
+        private ServiceProvider provider;
+
+        public PresenterFactory(IServiceProvider provider)
+        {
+            this.provider = (ServiceProvider)provider;
+        }
+        public TPresenter Create<TPresenter>(object view)
+        {
+            var descriptor = provider.KeyValuePairs[typeof(TPresenter)].Last();
+            Type type = descriptor.ImplementationType != null ? descriptor.ImplementationType : descriptor.KeyedImplementationType;
+            foreach (var item in type.GetConstructors().OrderByDescending(x => x.GetParameters().Length))
+            {
+                object[] args = item.GetParameters().Select(x =>
+                { if (x.ParameterType.IsInstanceOfType(view)) { return view; }; return provider.GetService(x.ParameterType); }
+                ).ToArray();
+                return (TPresenter)Activator.CreateInstance(type, args);
+            }
+            return (TPresenter)Activator.CreateInstance(type, view);
+        }
+    }
+}
